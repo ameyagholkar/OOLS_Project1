16f310715321dbdb80f3de7a7787d950a1871dfd
Ameya Gholkar <ameya.moses@gmail.com>
2011-09-17 16:03:05 -0400

    New Porject Initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveQuestionTool::Application.config.secret_token = '220811e106f97ce4f360df18b9436e5b4be896f92f547b2847911e9ea094438288be3dd6e8c9d70d07ce2cfa86a57200c1e4b9f37733c034e26ce95b5c346936'
