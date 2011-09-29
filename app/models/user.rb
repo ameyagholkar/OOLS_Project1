1b78450c4b0513347efd4e030a533683329f7f1d
Ameya Gholkar <ameya.moses@gmail.com>
2011-09-28 22:50:01 -0400

    Added Password Hashing. Once you download this code, the earlier Database with string passwords have to be cleared. They wont work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,25 @@
+require 'digest'
 class User < ActiveRecord::Base
-  has_many :posts
-  has_many :votes
+
 
   validates_presence_of :username ,:password , :email ,:name
   validates_uniqueness_of :username
-  validates_length_of :password, :within=> 4..15 ,:too_long => "password should contain less than 20 characters" ,:too_short => "password shoud contain more than 4 characters "
+  validates_length_of :password, :within=> 4..15 ,:too_long => "password should contain less than 20 characters" ,:too_short => "password should contain more than 4 characters "
   validates_format_of :email,:with => /\b[A-Z0-9._%a-z-]+@(?:[A-Z0-9a-z-]+.)+[A-Za-z]{2,4}\z/
 
+  #validates :password, :confirmation => true
+  before_save :encryption
+
+  def encryption
+       self.password = hash_value(password)
+  end
+
+  def hash_value(password)
+    Digest::SHA2.hexdigest(password)
+  end
+
   def has_password?(user_password)
-    password == user_password
+    password == hash_value(user_password)
   end
 
   def self.check_password(user_username, user_password)
@@ -24,4 +35,3 @@ class User < ActiveRecord::Base
       end
   end
 end
-
