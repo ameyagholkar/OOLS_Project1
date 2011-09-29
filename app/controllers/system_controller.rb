0859ba14691002f45758e7d3b31417bea5a071ca
Ameya Gholkar <ameya.moses@gmail.com>
2011-09-29 17:11:29 -0400

    Akshay - Voting functionality - Multiple votes handler.

--- app/controllers/system_controller.rb
+++ app/controllers/system_controller.rb
@@ -4,19 +4,31 @@ class SystemController < ApplicationController
   end
 
   def add_vote
-    post = Post.find(params[:id])
-    if post.users_id == params[:user_id]
-      flash[:error] = "You cannot vote for your own Post!"
-    else
-      post.num_of_votes = post.num_of_votes + 1
-      post.save
-      vote = Vote.new
-      vote.posts_id = params[:id]
-      vote.users_id = params[:user_id]
-      vote.save
-      redirect_to :action => 'index'
-    end
+      post = Post.find(params[:id])
+      vote1 = Vote.find_by_posts_id_and_users_id(Integer(params[:id]),Integer(params[:user_id]))
+      vote_user = Post.find_by_users_id(Integer(params[:user_id]))
+      flag = 0
+      if vote_user.nil? == false
+      if post.users_id == vote_user.users_id
+        flag = 1
+      end
+      end
+      if vote1.nil? == true && flag == 0
+        post.num_of_votes = post.num_of_votes + 1
+        post.save
+        vote = Vote.new
+        vote.posts_id = params[:id]
+        vote.users_id = params[:user_id]
+        vote.save
+        redirect_to  '/liveQuestions'
 
+      elsif flag !=1
+        flash[:error] = "You cannot vote more than once"
+        redirect_to  '/liveQuestions'
+      else
+        flash[:error] =  "You cannot vote your own post"
+        redirect_to  '/liveQuestions'
+      end
   end
 
   def add_post
@@ -25,7 +37,7 @@ class SystemController < ApplicationController
     @p.description=@desc
     @p.users_id = params[:user_id]
     @p.save
-    redirect_to :action => 'index'
+    redirect_to  '/liveQuestions'
   end
 
   def add_reply
@@ -35,6 +47,6 @@ class SystemController < ApplicationController
     @p.users_id = params[:user_id]
     @p.parent = params[:post_id]
     @p.save
-    redirect_to :action => 'index'
+    redirect_to  '/liveQuestions'
   end
 end
