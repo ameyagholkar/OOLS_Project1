6ced46f1affc7e82fab2d3b32b6d3c3b7434c7e7
Ryan Hasian <rkhasian@ncsu.edu>
2011-10-02 19:45:16 -0400

    Now sort posts according to vote_frequency, which is votes/time.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,15 +6,21 @@ class Post < ActiveRecord::Base
   validates_presence_of :description
 
   def self.find_top_posts
-    all(:order => "num_of_votes DESC", :conditions => {:parent => -1} )
+    result = all(:conditions => {:parent => -1} )
+    result.sort {|a,b| vote_frequency(b) <=> vote_frequency(a)};
   end
 
   def self.find_replies(p)
     all(:order => "num_of_votes DESC", :conditions => {:parent => p} )
   end
 
+  def self.vote_frequency(p)
+    p.num_of_votes/(Time.now - p.created_at)
+  end
+
   def self.post_search(text)
-    all(:order => "num_of_votes DESC",:conditions => ['parent = -1 AND description LIKE ?', "%" + text + "%"])
+    result = all(:conditions => ['parent = -1 AND description LIKE ?', "%" + text + "%"])
+    result.sort {|a,b| vote_frequency(b) <=> vote_frequency(a)};
   end
 
   def self.user_search(text)
@@ -23,6 +29,7 @@ class Post < ActiveRecord::Base
     if user
       user_id = user.id
     end
-    all(:order => "num_of_votes DESC",:conditions => {:users_id => user_id, :parent => -1})
+    result = all(:conditions => {:users_id => user_id, :parent => -1})
+    result.sort {|a,b| vote_frequency(b) <=> vote_frequency(a)};
   end
 end
