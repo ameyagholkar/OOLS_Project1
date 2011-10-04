6813b0713ce2b722c5a8d43e4ce102adf141fe73
Ameya Gholkar <ameya.moses@gmail.com>
2011-10-03 22:38:47 -0400

    Added Statistical Tools.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,34 @@
 module UsersHelper
+
+  def get_number_of_votes(user)
+    @posts = Post.find_all_by_users_id(user)
+    if @posts.nil?
+      "No Posts Found!"
+    else
+      num_of_votes = 0
+      @posts.each do |post|
+        num_of_votes = num_of_votes + post.num_of_votes
+      end
+      num_of_votes
+    end
+  end
+
+  def get_posting_frequency(user)
+    num_of_posts = Post.find_all_by_users_id(user.id).count
+    if num_of_posts == 0
+      "No posting frequency data available."
+    else
+      hours = Integer((Time.now - @user.created_at.in_time_zone('Eastern Time (US & Canada)'))/3600)
+      minutes = Integer((Time.now - @user.created_at.in_time_zone('Eastern Time (US & Canada)'))/(3600)*100)
+      if hours > 24
+         days = Integer(hours/24)
+         "#{num_of_posts} posts in #{days} days"
+      elsif hours == 0
+         "#{num_of_posts} posts in #{minutes} minutes"
+      else
+         "#{num_of_posts} posts in #{hours} hours"
+      end
+    end
+  end
+
 end
